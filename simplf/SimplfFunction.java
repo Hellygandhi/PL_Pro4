692b49c0f349cb15ed64dc1b96dd468446de4057
Kush gandhi <127643258+Kushgandhi18@users.noreply.github.com>
2025-04-11 22:00:43 -0400

    Update SimplfFunction.java

--- simplf/SimplfFunction.java
+++ simplf/SimplfFunction.java
@@ -15,35 +15,28 @@ class SimplfFunction implements SimplfCallable {
         this.closure = environment;
     }
 
+    @Override
     public Object call(Interpreter interpreter, List<Object> args) {
-        if (args.size() != declaration.params.size()) {
-            throw new RuntimeError(null, "Expected " + declaration.params.size() + " arguments but got " + args.size() + ".");
-        }
-    
-        Environment environment = new Environment(this.closure);
-        environment = environment.define(declaration.name, declaration.name.lexeme, this);
-    
+        Environment env = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
-            Object arg = args.get(i);
-            if (arg instanceof Number) {
-                arg = ((Number)arg).doubleValue();
-            }
-            environment = environment.define(
-                declaration.params.get(i),
-                declaration.params.get(i).lexeme,
-                arg
-            );
+            env = env.define(declaration.params.get(i), declaration.params.get(i).lexeme, args.get(i));
         }
-    
-        try {
-            interpreter.executeBlock(declaration.body, environment);
-        } catch (Return returnValue) {
-            return returnValue.value;
+
+        Object result = null;
+        for (Stmt stmt : declaration.body) {
+            result = interpreter.execute(stmt);
         }
+
+        return result;
     }
 
     @Override
     public String toString() {
-        return "<fn " + (declaration.name != null ? declaration.name.lexeme : "") + ">";
+        return "<fn " + declaration.name.lexeme + ">";
+    }
+
+    @Override
+    public int arity() {
+        return declaration.params.size();
     }
 }
